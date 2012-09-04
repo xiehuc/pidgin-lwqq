e4fec991a617fbe5a430762abd4b146238cf7e8d
Felix Yan <felixonmars@gmail.com>
2012-09-04 11:24:59 +0800

    add disable_custom_font_size option

--- src/webqq.c
+++ src/webqq.c
@@ -1275,6 +1275,7 @@ init_plugin(PurplePlugin *plugin)
     PurpleAccountOption *option;
     option = purple_account_option_bool_new("禁用自定义接收消息字体大小", "disable_custom_font_size", FALSE);
     webqq_prpl_info.protocol_options = g_list_append(webqq_prpl_info.protocol_options, option);
+    purple_prefs_add_none("/plugins/prpl/webqq");
     
 #ifdef ENABLE_NLS
     setlocale(LC_ALL, "");
