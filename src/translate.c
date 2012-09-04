ce1e0a560ea6364c2e0335ce1de0bd2c4e5bcd5d
Felix Yan <felixonmars@gmail.com>
2012-09-04 12:26:52 +0800

    fix a typo

--- src/translate.c
+++ src/translate.c
@@ -229,7 +229,7 @@ void translate_struct_to_message(qq_account* ac, LwqqMsgMessage* msg, char* buf)
     if(msg->f_style.u==1) strcat(buf,"<u>");
     if(!ac->disable_custom_font_size)
         snprintf(buf+strlen(buf),300,"<font size=\"%d\" face=\"%s\" color=\"#%s\">",sizeunmap(msg->f_size),msg->f_name,msg->f_color);
-    else:
+    else
         snprintf(buf+strlen(buf),300,"<font face=\"%s\" color=\"#%s\">",msg->f_name,msg->f_color);
     
     TAILQ_FOREACH(c, &msg->content, entries) {
