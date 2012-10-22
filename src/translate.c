c5d10ef614f24b9184d23260b9958191ec5dea37
Felix Yan <felixonmars@gmail.com>
2012-10-22 11:31:25 +0800

    Fix dark theme

--- src/translate.c
+++ src/translate.c
@@ -231,9 +231,9 @@ void translate_struct_to_message(qq_account* ac, LwqqMsgMessage* msg, char* buf)
     if(msg->f_style.u==1) strcat(buf,"<u>");
     snprintf(buf+strlen(buf),300,"<font ");
     if(ac->dark_theme_fix && !strcmp("000000", msg->f_color))
-        snprintf(buf+strlen(buf),300,"\"#ffffff\" ");
+        snprintf(buf+strlen(buf),300,"color=\"#ffffff\" ");
     else
-        snprintf(buf+strlen(buf),300,"\"#%s\" ",msg->f_color);
+        snprintf(buf+strlen(buf),300,"color=\"#%s\" ",msg->f_color);
     if(!ac->disable_custom_font_face)
         snprintf(buf+strlen(buf),300,"face=\"%s\" ",msg->f_name);
     if(!ac->disable_custom_font_size)
