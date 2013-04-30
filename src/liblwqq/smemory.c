4be1ab0bf897035dc302f25fec8b0422df67a92b
xiehuc <xiehuc@gmail.com>
2013-04-30 10:58:32 +0800

    clean smemory
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- src/liblwqq/smemory.c
+++ src/liblwqq/smemory.c
@@ -14,57 +14,6 @@
 #include <stdarg.h>
 #include <string.h>
 
-void *s_malloc(size_t size)
-{
-	if(size == 0)
-		return NULL;
-
-    return malloc(size);
-}
-
-void *s_malloc0(size_t size)
-{
-    if (size == 0)
-        return NULL;
-    
-    void *ptr = malloc(size);
-    if (ptr)
-        memset(ptr, 0, size);
-    
-    return ptr;
-}
-
-/*j
-void s_free(void* ptr)
-{
-    if(ptr) free(ptr);
-}*/
-
-void *s_calloc(size_t nmemb, size_t lsize)
-{
-    return calloc(nmemb, lsize);
-}
-
-void *s_realloc(void *ptr, size_t size)
-{
-    return realloc(ptr, size);
-}
-
-char *s_strdup(const char *s1)
-{
-    if (!s1)
-        return NULL;
-
-    return strdup(s1);
-}
-
-char *s_strndup(const char *s1, size_t n)
-{
-    if (!s1)
-        return NULL;
-
-    return strndup(s1, n);
-}
 
 int s_vasprintf(char **buf, const char * format,
 				 va_list arg)
@@ -82,14 +31,4 @@ int s_asprintf(char **buf, const char *format, ...)
 	va_end(arg);
 
     return rv;
-}
-int s_atoi(const char* s,int init)
-{
-    if(s) return strtol(s,NULL,10);
-    return init;
-}
-int s_atol(const char* s,int init)
-{
-    if(s) return strtol(s,NULL,10);
-    return init;
 }
