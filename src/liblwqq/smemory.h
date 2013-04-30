6c4e8622778ab06d751a494c330bee91680c3ba2
xiehuc <xiehuc@gmail.com>
2013-04-30 12:41:47 +0800

    Revert "clean smemory"
    This reverts commit 4be1ab0bf897035dc302f25fec8b0422df67a92b.

--- src/liblwqq/smemory.h
+++ src/liblwqq/smemory.h
@@ -16,16 +16,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void *s_malloc(size_t size);
+void *s_malloc0(size_t size);
+void *s_calloc(size_t nmemb, size_t lsize);
+void *s_realloc(void *ptr, size_t size);
+//void s_free(void *p);
+char *s_strdup(const char *s1);
+char *s_strndup(const char *s1, size_t n);
 int s_vasprintf(char **buf, const char * format, va_list arg);
 int s_asprintf(char **buf, const char *format, ...);
-#define s_malloc(sz) ((sz==0)?NULL:malloc(sz))
-#define s_malloc0(sz) ((sz==0)?NULL:memset(malloc(sz),0,sz))
-#define s_calloc(num,sz) calloc(num,sz)
-#define s_realloc(ptr,sz) realloc(ptr,sz)
-#define s_strdup(s) (s!=NULL?strdup(s):NULL)
-#define s_strndup(s,n)  (s!=NULL?strndup(s,n):NULL)
-#define s_atol(s,init) (s!=NULL?strtol(s,NULL,10):init)
-#define s_atoi(s,init) s_atol(s,init)
-#define s_free(p) (p=(p!=NULL)?free(p),NULL:NULL)
+int s_atoi(const char* s,int init);
+int s_atol(const char* s,int init);
+#define s_free(p) \
+do{ \
+    if(p) free(p);\
+    p = NULL;\
+}while(0)
 
 #endif  /* SMEMORY_H */
